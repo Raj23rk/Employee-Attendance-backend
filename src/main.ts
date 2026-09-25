83e8e2b79f52691e792849f723143a014fdbc46b
Rajkumar <kumarrk23dev@gmail.com>
2026-09-25 13:59:39 +0530

    new changes

--- src/main.ts
+++ src/main.ts
@@ -50,7 +50,7 @@ async function bootstrap() {
   });
 
   const port = process.env.PORT || 5000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   logger.log(`=======================================================`);
   logger.log(`🚀 Employee Attendance Backend running on port ${port}`);
